c575a7409386c5b4e779bb592de9a32c7d0271d0
Harun Cetin <hcetin01@hotmail.com>
2024-01-12 00:26:25 +0300

    dares.py updated

--- model/siameseNet/dares.py
+++ model/siameseNet/dares.py
@@ -6,8 +6,7 @@
 from __future__ import division
 
 import sys
-#sys.path.append("D:\Works\PhD\Machine Learning\Project\DASNet_CD\DASNet\model\siameseNet")
-sys.path.append("/condent/DASNet/model/siameseNet")
+sys.path.append("/content/DASNet/model/siameseNet")
 
 import os
 import numpy as np
